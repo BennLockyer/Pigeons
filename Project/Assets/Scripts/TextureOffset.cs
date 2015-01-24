d7ff7df95eba98527c50cc42b1a198c565c344d5
Benn Lockyer <benn.lockyer@hotmail.com>
2015-01-25 02:26:26 +1100

    lots of updates

---
+++ Project/Assets/Scripts/TextureOffset.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureOffset : MonoBehaviour 
+{
+	public float scrollSpeed;
+	
+	void Update () 
+	{	
+		float offset = scrollSpeed * Time.time;
+		renderer.material.SetTextureOffset("_MainTex",new Vector2(offset,0));
+	}
+}
