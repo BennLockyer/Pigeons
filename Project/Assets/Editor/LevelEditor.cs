5af1922339981d5fa87edb7fa135dfaac875ce8c
Benn Lockyer <benn.lockyer@hotmail.com>
2015-01-24 11:44:43 +1100

    Updated level editor to created models.

--- Project/Assets/Editor/LevelEditor.cs
+++ Project/Assets/Editor/LevelEditor.cs
@@ -6,7 +6,6 @@ public class LevelEditor : EditorWindow
 {
 	[MenuItem("GameJam/Level Editor")]
 	
-	
 	public static void ShowWindow()
 	{
 		EditorWindow.GetWindow(typeof(LevelEditor));
@@ -32,6 +31,14 @@ public class LevelEditor : EditorWindow
 	private Material deadend270 = null;
 	private Material[] materials = new Material[16];
 	
+	private int xOffset;
+	private int yOffset;
+	
+	private bool checkClear = false;
+	private bool checkGrid = false;
+	private bool checkGenerate = false;
+	
+	private string warningText = "This is a message";
 	private GameObject tile;
 	
 	void OnEnable()
@@ -44,22 +51,22 @@ public class LevelEditor : EditorWindow
 			}
 		}
 		
-		empty = Resources.Load<Material>("m_empty");
-		corner = Resources.Load<Material>("m_corner");
-		corner90 = Resources.Load<Material>("m_corner90");
-		corner180 = Resources.Load<Material>("m_corner180");
-		corner270 = Resources.Load<Material>("m_corner270");
-		straight = Resources.Load<Material>("m_straight");
-		straight90 = Resources.Load<Material>("m_straight90");
-		threeway = Resources.Load<Material>("m_3way");
-		threeway90 = Resources.Load<Material>("m_3way90");
-		threeway180 = Resources.Load<Material>("m_3way180");
-		threeway270 = Resources.Load<Material>("m_3way270");
-		cross = Resources.Load<Material>("m_cross");
-		deadend = Resources.Load<Material>("m_deadend");
-		deadend90 = Resources.Load<Material>("m_deadend90");
-		deadend180 = Resources.Load<Material>("m_deadend180");
-		deadend270 = Resources.Load<Material>("m_deadend270");
+		empty = Resources.Load<Material>("Materials/m_empty");
+		corner = Resources.Load<Material>("Materials/m_corner");
+		corner90 = Resources.Load<Material>("Materials/m_corner90");
+		corner180 = Resources.Load<Material>("Materials/m_corner180");
+		corner270 = Resources.Load<Material>("Materials/m_corner270");
+		straight = Resources.Load<Material>("Materials/m_straight");
+		straight90 = Resources.Load<Material>("Materials/m_straight90");
+		threeway = Resources.Load<Material>("Materials/m_3way");
+		threeway90 = Resources.Load<Material>("Materials/m_3way90");
+		threeway180 = Resources.Load<Material>("Materials/m_3way180");
+		threeway270 = Resources.Load<Material>("Materials/m_3way270");
+		cross = Resources.Load<Material>("Materials/m_cross");
+		deadend = Resources.Load<Material>("Materials/m_deadend");
+		deadend90 = Resources.Load<Material>("Materials/m_deadend90");
+		deadend180 = Resources.Load<Material>("Materials/m_deadend180");
+		deadend270 = Resources.Load<Material>("Materials/m_deadend270");
 		materials[0] = empty;
 		materials[1] = corner;
 		materials[2] = corner90;
@@ -118,21 +125,67 @@ public class LevelEditor : EditorWindow
 			}
 			GUILayout.EndHorizontal();
 		}
-		
-		if(GUILayout.Button("Generate",GUILayout.Width(100),GUILayout.Height(30)))
-		{
-			GenerateMap();
-		}
-		
 		GUILayout.BeginHorizontal();
-		if(GUILayout.Button("Clear Grid",GUILayout.Width(100),GUILayout.Height(30)))
+		xOffset = EditorGUILayout.IntField("x offset: ",xOffset,GUILayout.Width(200));
+		yOffset = EditorGUILayout.IntField("y offset: ",yOffset,GUILayout.Width(200));
+		GUILayout.EndHorizontal();
+		GUILayout.BeginHorizontal();
+		if(!checkClear && !checkGrid && !checkGenerate)
 		{
-			ClearGrid();
+			if(GUILayout.Button("Generate",GUILayout.Width(100),GUILayout.Height(30)))
+			{
+				checkGenerate = true;
+				warningText = "Generate the grid into the game?";
+			}
+			
+			
+			if(GUILayout.Button("Clear Grid",GUILayout.Width(100),GUILayout.Height(30)))
+			{
+				checkGrid = true;
+				warningText = "Are you sure you want to clear the grid?";
+			}
+			if(GUILayout.Button("Clear Scene",GUILayout.Width(100),GUILayout.Height(30)))
+			{
+				checkClear = true;
+				warningText = "Are you sure you want to clear the scene?";
+			}
 		}
-		if(GUILayout.Button("Clear Scene",GUILayout.Width(100),GUILayout.Height(30)))
+		else
 		{
-			ClearScene();
+			if(GUILayout.Button("YES",GUILayout.Width(100),GUILayout.Height(30)))
+			{
+				if(checkGrid)
+				{
+					checkGrid = false;
+					ClearGrid();
+					warningText = "The grid has been cleared.";
+				}
+				if(checkClear)
+				{
+					checkClear = false;
+					ClearScene();
+					warningText = "The scene has been cleared.";
+				}
+				if(checkGenerate)
+				{
+					checkGenerate = false;
+					GenerateMap();
+					warningText = "The grid has been generated.";
+				}
+			}
+			
+			if(GUILayout.Button("NO",GUILayout.Width(100),GUILayout.Height(30)))
+			{
+				checkGrid = false;
+				checkClear = false;
+				checkGenerate = false;
+				warningText = "No changes have been made.";
+			}
 		}
+		GUILayout.EndHorizontal();
+		GUILayout.BeginHorizontal();
+		GUI.color = Color.yellow;
+		GUILayout.Label(warningText,GUILayout.Width(400));
 	}
 	
 	void GenerateMap()
@@ -143,8 +196,71 @@ public class LevelEditor : EditorWindow
 			{
 				if(index[x,y] > 0)
 				{
-					GameObject t = (GameObject)Instantiate(tile,new Vector3(x,0,y),Quaternion.identity);
-					t.renderer.material = materials[index[x,y]];
+					string modelName = "";
+					int rotation = 0;
+					switch(index[x,y])
+					{
+					case 1:
+						modelName = "model_corner";
+						rotation = 180;
+						break;
+					case 2:
+						modelName = "model_corner";
+						rotation = 270;
+						break;
+					case 3:
+						modelName = "model_corner";
+						break;
+					case 4:
+						modelName = "model_corner";
+						rotation = 90;
+						break;
+					case 5:
+						modelName = "model_straight";
+						rotation = 90;
+						break;
+					case 6:
+						modelName = "model_straight";
+						break;
+					case 7:
+						modelName = "model_t";
+						rotation = 90;
+						break;
+					case 8:
+						modelName = "model_t";
+						rotation = 180;
+						break;
+					case 9:
+						modelName = "model_t";
+						rotation = 270;
+						break;
+					case 10:
+						modelName = "model_t";
+						break;
+					case 11:
+						modelName = "model_cross";
+						break;
+					case 12:
+						modelName = "model_end";
+						rotation = 180;
+						break;
+					case 13:
+						modelName = "model_end";
+						rotation = 270;
+						break;
+					case 14:
+						modelName = "model_end";
+						break;
+					case 15:
+						modelName = "model_end";
+						rotation = 90;
+						break;
+					}
+					GameObject t = Resources.Load<GameObject>("Tiles/" + modelName);
+					GameObject u = (GameObject)Instantiate (t,new Vector3(x+(16*xOffset),0,y+(16*yOffset)),Quaternion.identity);
+					u.transform.eulerAngles = new Vector3(0,rotation,0);
+//					GameObject t = (GameObject)Instantiate(tile,new Vector3(x+(16*xOffset),0,y+(16*yOffset)),Quaternion.identity);
+//					t.renderer.material = materials[index[x,y]];
 				}
 			}
 		}
